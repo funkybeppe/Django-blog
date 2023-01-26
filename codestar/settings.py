8108dd4f2b60df573614e0e0f1f3bc339a6312c0
funkybeppe <lucabarbero1996@gmail.com>
2023-01-26 13:22:14 +0000

    Deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -76,7 +76,7 @@ SOCIALACCOUNT_PROVIDERS = {
             'email',
         ],
         'AUTH_PARAMS': {
-            'access_type': 'offline',
+            'access_type': 'online',
         },
         'OAUTH_PKCE_ENABLED': True,
     }
