70ad6932081ed33c7eaf15ff95edb2ebb47c9c9f
funkybeppe <lucabarbero1996@gmail.com>
2023-01-26 09:57:44 +0000

    Add google account sign in

--- codestar/settings.py
+++ codestar/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
@@ -59,6 +60,19 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        },
+        'OAUTH_PKCE_ENABLED': True,
+    }
+}
 
 MESSAGE_TAGS = {
         messages.DEBUG: 'alert-info',
